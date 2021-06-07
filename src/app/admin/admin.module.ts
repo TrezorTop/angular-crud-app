d0bb062c6a1a0003be025cc36b32a23213df92f0
Andrey Seluykov <trezor6@mail.ru>
2021-06-07 19:04:55 +0300

    admin layout design

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,7 +3,6 @@ import {CommonModule} from "@angular/common";
 import {RouterModule} from "@angular/router";
 import {AdminLayoutComponent} from './shared/components/admin-layout/admin-layout.component';
 import { MainPageComponent } from './main-page/main-page.component';
-import {HomePageComponent} from "../home-page/home-page.component";
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { CreatePageComponent } from './create-page/create-page.component';
 import { EditPageComponent } from './edit-page/edit-page.component';
@@ -21,8 +20,8 @@ import { EditPageComponent } from './edit-page/edit-page.component';
     RouterModule.forChild([
       {
         path: '', component: AdminLayoutComponent, children: [
-          {path: '', redirectTo: '/admin/main', pathMatch: 'full'},
-          {path: 'main', component: HomePageComponent},
+          {path: '', redirectTo: '/admin/dashboard', pathMatch: 'full'},
+          {path: 'main', component: MainPageComponent},
           {path: 'dashboard', component: DashboardPageComponent},
           {path: 'create', component: CreatePageComponent},
           {path: 'event/:id/edit', component: EditPageComponent},
