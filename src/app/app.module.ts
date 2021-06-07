5c4022c08053e00567881951227d1f7e1e3e3aca
Andrey Seluykov <trezor6@mail.ru>
2021-06-07 17:50:15 +0300

    main page design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { EventPageComponent } from './event-page/event-page.component';
+import { EventComponent } from './shared/components/event/event.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainLayoutComponent,
     HomePageComponent,
-    EventPageComponent
+    EventPageComponent,
+    EventComponent
   ],
   imports: [
     BrowserModule,
