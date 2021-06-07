951b04038a3af842e3136db94e3958b7b1c7086e
Andrey Seluykov <trezor6@mail.ru>
2021-06-07 17:34:48 +0300

    added admin layout

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,39 @@
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {RouterModule} from "@angular/router";
+import {AdminLayoutComponent} from './shared/components/admin-layout/admin-layout.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import {HomePageComponent} from "../home-page/home-page.component";
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { CreatePageComponent } from './create-page/create-page.component';
+import { EditPageComponent } from './edit-page/edit-page.component';
+
+@NgModule({
+  declarations: [
+    AdminLayoutComponent,
+    MainPageComponent,
+    DashboardPageComponent,
+    CreatePageComponent,
+    EditPageComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: '', component: AdminLayoutComponent, children: [
+          {path: '', redirectTo: '/admin/main', pathMatch: 'full'},
+          {path: 'main', component: HomePageComponent},
+          {path: 'dashboard', component: DashboardPageComponent},
+          {path: 'create', component: CreatePageComponent},
+          {path: 'event/:id/edit', component: EditPageComponent},
+        ]
+      }
+    ])
+  ],
+  exports: [
+    RouterModule
+  ],
+})
+export class AdminModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { EventPageComponent } from './event-page/event-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainLayoutComponent
+    MainLayoutComponent,
+    HomePageComponent,
+    EventPageComponent
   ],
   imports: [
     BrowserModule,
